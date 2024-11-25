851f6380758a10045f13fd99e56c44c3aedebe02
wfox5510 <wfox5510@gmail.com>
2024-11-26 00:37:59 +0800

    complete render cartList

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,13 +2,19 @@ import { api_base, api_path } from '../../main'
 
 axios.get(`${api_base}api/livejs/v1/customer/${api_path}/products`)
   .then((response) => {
-    console.log(response);
-    getProductsList(response.data.products);
+    renderProductsList(response.data.products);
+  })
+  .catch((error) => console.log(error.response.data.messege || '取得產品列表失敗'))
+
+axios.get(`${api_base}api/livejs/v1/customer/${api_path}/carts`)
+  .then((response) => {
+    console.log(response.data);
+    renderCartsList(response.data);
   })
   .catch((error) => console.log(error.response.data.messege || '取得產品列表失敗'))
 
 let productWrap = document.querySelector('.productWrap');
-function getProductsList(productsData) {
+function renderProductsList(productsData) {
   let productCardHtml;
   let productList = productsData.map((productsItem) => {
     productCardHtml = `<li class="productCard">
@@ -25,4 +31,47 @@ function getProductsList(productsData) {
   }).join('');
 
   productWrap.innerHTML = productList;
+}
+let shoppingCartTable = document.querySelector('.shoppingCart-table');
+function renderCartsList(shoppingCartData) {
+  let shoppingCartList = "";
+  shoppingCartList += `<tr>
+          <th width="40%">品項</th>
+          <th width="15%">單價</th>
+          <th width="15%">數量</th>
+          <th width="15%">金額</th>
+          <th width="15%"></th>
+        </tr>`;
+  shoppingCartList += shoppingCartData.carts.map((shoppingCartItem) => {
+    let shoppingCartHtml = `<tr>
+          <td>
+            <div class="cardItem-title">
+              <img src="https://i.imgur.com/HvT3zlU.png" alt="">
+              <p>Antony ${shoppingCartItem.product.title}</p>
+            </div>
+          </td>
+          <td>${shoppingCartItem.product.price}</td>
+          <td>${shoppingCartItem.quantity}</td>
+          <td>${shoppingCartItem.product.price * shoppingCartItem.quantity}</td>
+          <td class="discardBtn">
+            <a href="#" class="material-icons">
+              clear
+            </a>
+          </td>
+        </tr>`;
+    return shoppingCartHtml;
+  }).join('');
+  shoppingCartList += `<tr>
+          <td>
+            <a href="#" class="discardAllBtn">刪除所有品項</a>
+          </td>
+          <td></td>
+          <td></td>
+          <td>
+            <p>總金額</p>
+          </td>
+          <td>NT$${shoppingCartData.finalTotal}</td>
+        </tr>`
+  console.log(shoppingCartList);
+  shoppingCartTable.innerHTML = shoppingCartList;
 }
